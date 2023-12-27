426c0bf58b205c9d5e9f138b5624b18b360e699b
nickerick <nickerickson737@gmail.com>
2023-12-27 01:04:11 -0600

    add error handling page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="container">
+	<h1>{$page.status} {$page?.error?.message ?? 'Error'}</h1>
+</div>
+
+<style>
+	.container {
+		margin: 10vh auto;
+		width: 25%;
+		height: 20vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #fefdfd;
+		border-radius: 1em;
+	}
+</style>
