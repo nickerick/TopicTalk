45624fb819e3d8ef5ce0e2453c8562ea2c0fc3b7
nickeric <nickerickson737@gmail.com>
2023-12-26 22:08:16 -0600

    initialize project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import logo from '$lib/images/topic-talk.png';
+</script>
+
+<nav class="navbar">
+    <div class="logo">
+        <img src={logo} alt="topic-talk-logo">
+    </div>
+    <div class="nav-links">
+        <button>About</button>
+        <button>New Topic!</button>
+    </div>
+
+</nav>
